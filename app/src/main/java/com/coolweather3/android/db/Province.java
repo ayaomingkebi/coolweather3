3b6166c279028e4869b40d6f26b6379ca4988b22
Tony <tony@gmail.com>
2017-01-22 10:15:00 +0800

    Second commit

---
+++ app/src/main/java/com/coolweather3/android/db/Province.java
@@ -0,0 +1,26 @@
+package com.coolweather3.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/1/21 0021.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+}
+
