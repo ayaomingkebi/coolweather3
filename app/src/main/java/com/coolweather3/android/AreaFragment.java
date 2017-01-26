b0d123e323900cf0b366e047f3f167a6fcd95bc7
Tony <tony@gmail.com>
2017-01-26 21:30:12 +0800

    Fifth commit

--- app/src/main/java/com/coolweather3/android/AreaFragment.java
+++ app/src/main/java/com/coolweather3/android/AreaFragment.java
@@ -74,10 +74,18 @@ public class AreaFragment extends Fragment {
                     queryCounties();
                 }else if(currentlevel==LEVEL_COUNTY){
                     String weatherId = countryList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if(getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if(getActivity() instanceof  WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+
+                    }
                 }
             }
         });
